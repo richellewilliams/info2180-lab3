491f7a82d4478d9930e2dc72dc3959e9887a66c1
Richelle Williams <richellewilliams3@gmail.com>
2022-10-11 12:02:30 -0500

    Added code so that X or O appears to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,33 @@
 "use strict";
 
 window.onload = function() {
+
     const board = document.querySelector("#board");
     
     for (var i = 0; i < board.children.length; i++) {
         var individualSquare = board.children[i];
         individualSquare.classList.add('square');
+    }
+
+    for (var i = 0; i < board.children.length; i++) {
+        var individualSquare = board.children[i]
+        individualSquare.addEventListener('click', X_or_O);
+    }
+
+    const options = [];
+    let index = 0;
+
+    function X_or_O() {
+          if (index % 2 == 0) {
+                options.push("X");
+                this.textContent = options[index];
+                this.classList.add(options[index]);
+         
+        } else if (index % 2 == 1) {
+                options.push("O");
+                this.textContent = "O";
+                this.classList.add(options[index]);
+        } 
+        index++;
     }
 }
